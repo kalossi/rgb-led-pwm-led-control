5a7f1d76dd27286475038e063515be702eb998dc
Ville <kalossi@gmail.com>
2020-11-14 17:33:13 +0200

    pwm does what it should

--- main.c
+++ main.c
@@ -1,35 +1,37 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <math.h>
+// #include <stdio.h>
+// #include <math.h>
 
 void initADC()
 {
 // Select Vref=AVcc and left adjust ADC register
 	ADMUX |= (0 << REFS1) | (1 << REFS0) | (1 << ADLAR);
-//set prescaller to 128 and enable ADC
-	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);
+//set prescaller to 8, enable ADC, ADC interrupts and auto trigger
+	ADCSRA |= (0 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN) | (1 << ADATE) | (1 << ADIE);
 }
 
 void initPWM()
 {
-// Fast PWM: WGM22, WGM21 and WGM20 = 111, clear OC2A on compare match
+// PWM phase correct: WGM22, WGM21 and WGM20 = 101, clear OC2A on compare match
 	TCCR2A |= (1 << COM2A1) | (1 << COM2A0) | (0 << WGM21) | (1 << WGM20) | (1 << COM2A1) | (0 << COM2A0);
 	TCCR2B |= (1 << WGM22);
 // No prescaler: CS22, CS21 and CS20 = 001
 	TCCR2B |= (1 << CS20);
-	TCNT2 |= 0x00;
+	TIMSK2 |= (1 << OCIE2A);
+	OCR2A = 5;
 }
 
 uint8_t readADCH(uint8_t ADCchannel)
 {
 //select ADC channel with safety mask
 	ADMUX = (ADMUX & 0xf0) | (ADCchannel & 0x0f);
-//single conversion mode
-	ADCSRA |= (1 << ADSC); //start conversion
+//single conversion mode, ADC prescaler 8
+	ADCSRA |= (1 << ADSC) | (1 << ADPS1);  //start conversion
 // wait until ADC conversion is complete
-	while (!(ADCSRA & (1 << ADIF))) {
-	}
+	// while (!(ADCSRA & (1 << ADIF))) {
+	// }
 // clear flag
 	ADCSRA |= (1 << ADIF);
 // return the 8 msb of ADC register
@@ -38,14 +40,28 @@ uint8_t readADCH(uint8_t ADCchannel)
 
 int main()
 {
-	DDRB = 0xff;  // all outputs
+	DDRB = 0xff; // all outputs
 	PORTB = 0x00;
-	DDRC = 0x00; // all inputs
-	PORTC = 0xff;
+	DDRD = 0xff;  // all outputs
+	PORTD = 0xff; // active low leds
+	DDRC = 0x01; // PINC0 for a led
+	PORTC = 0x01;
 	initADC();
 	initPWM();
+	sei();
 // otherwise routine stops
 	while (1) {
-		OCR2A |= (readADCH(0));
+		// PORTD = ~(readADCH(0));
 	}
 }
+
+ISR (TIMER2_COMPA_vect)
+{
+// change the timer compare register 2A to match the 8 bits of ADCH register
+	OCR2A = readADCH(0);
+}
+
+// ISR (ADC_vect)
+// {
+// 	PORTD = ~(readADCH(0));
+// }
